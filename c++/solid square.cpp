d7eaae35b85db39a7c14861faaf9e68848683b18
AKSHAY <forakshay.dev@gmail.com>
2023-01-29 13:33:11 +0530

    3

--- c++/solid square.cpp
+++ c++/solid square.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-    for(int row =0; row<4; row =row+1 ){
-        for (int col =0;col<4;col=col+1){
+    int n;
+    cin>>n;
+    for(int row =0; row<n; row =row+1 ){
+        for (int col =0;col<n;col=col+1){
             cout<<"* ";
         }
         cout<<endl;
