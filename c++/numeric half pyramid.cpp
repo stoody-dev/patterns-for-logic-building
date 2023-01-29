328295a2c1a4e38d0c5ed065649cb7ee7cb012d9
AKSHAY <forakshay.dev@gmail.com>
2023-01-29 14:22:16 +0530

    5

---
+++ c++/numeric half pyramid.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    
+    for(int row =0; row<n; row =row+1 ){
+        for (int col =0;col<row+1;col=col+1){
+            cout<<col+1;
+        }
+        cout<<endl;
+
+    }
+}
