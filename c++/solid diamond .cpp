be8631bbdbebec3d4ba6734625bd411f2b1c201b
AKSHAY <forakshay.dev@gmail.com>
2023-02-03 21:47:24 +0530

    12

---
+++ c++/solid diamond .cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    //full pyramid 
+    for(int row=0;row<n;row=row+1){
+        for(int col=0;col<n-row-1;col =col+1){
+            cout<<" ";
+        }
+        for(int col=0;col<row+1;col =col+1 )
+            cout<<"* ";
+        cout<<endl;
+    }
+    //inverted full pyramid
+    for(int row=0;row<n;row =row+1 ){
+        for(int col=0;col<row;col =col+1){
+            cout<<" ";}
+
+
+    
+        for(int col=0;col<n-row;col =col+1){
+            cout<<"* ";
+        }
+        
+         cout<<endl;
+    }
+
+
+
+}
